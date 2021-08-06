06a0f2e4adc59dacba66052e2ffcad12a5671aeb
Reinaldo Duzanski Jr <reinaldoduzanskijr@gmail.com>
2021-08-05 22:37:57 -0300

    lista

---
+++ listas.py
@@ -0,0 +1,17 @@
+'''
+-> Fatiamento
+-> Comandos
+    -> Append
+    -> Insert
+    -> Pop
+    -> Del
+    -> Clear
+    -> Extend
+    -> Min
+    -> Max
+    -> Range
+'''
+
+# Criando lista e acessando valores
+lista = ['A', 'B', 'C', 'D']
+print(lista)
